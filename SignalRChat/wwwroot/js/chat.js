046b2d0b27860c6daf7c2a67019b19270ccdd2ad
Lisbaldy <lisba7474@gmail.com>
2020-08-08 18:38:45 -0300

    new logic and visual improvements

--- SignalRChat/wwwroot/js/chat.js
+++ SignalRChat/wwwroot/js/chat.js
@@ -1,48 +1,71 @@
 ﻿"use strict";
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+let connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
+let user;
+let modal = document.getElementById("modal");
 
-function scrollBottom() {
-    var messagesBox = document.getElementById("messagesList");
+function chooseNickname() {
+    document.getElementById("modal").style.display = "none";
+    user = document.getElementById("firstName").value;
+    document.getElementById('messageInput').style.backgroundColor = '#fff'
+}
+
+window.addEventListener('load', () => {
+    modal.style.display = "flex";
+    document.getElementById('messageInput').style.backgroundColor = '#aaa';
+});
+
+function scrollToBottom() {
+    let messagesBox = document.getElementById("messagesList");
     messagesBox.scrollTop = messagesBox.scrollHeight;
 }
 
 connection.on("ReceiveMessage", function (user, message) {
-    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says: " + msg;
-    var li = document.createElement("li");
+    let msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    let encodedMsg = user + " says: " + msg;
+    let li = document.createElement("li");
     li.textContent = encodedMsg;
     document.getElementById("messagesList").appendChild(li);
+    scrollToBottom();
 });
 
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
-});
+connection.start()
+    .then(function() {
+        document.getElementById("sendButton").disabled = false;
+    })
+    .catch(function(err) {
+        return console.error(err.toString());
+    });
 
 
 function send(event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+    let message = document.getElementById("messageInput").value;
+
+    connection.invoke("SendMessage", user, message)
+    .catch(function(err) {
         return console.error(err.toString());
     });
-    event.preventDefault();
 
+    event.preventDefault();
     document.getElementById("messageInput").value = "";
-    scrollBottom();
+    scrollToBottom();
 }
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
     send(event);
 });
 
-window.addEventListener("keydown", function (event) {
+window.addEventListener("keydown", function(event) {
     if (event.keyCode === 13) {
-        send(event);
+        if (modal.style.display === 'flex') {
+            chooseNickname()
+        }
+        else
+        {
+            send(event);
+        }
     }
 });
